12bb5959e58e4500ae52933fb9014bde6d0512f1
cobanyasin1998 <yasincoban1998@outlook.com>
2023-08-02 13:48:37 +0300

    redis publisher -subscriber

---
+++ Yazılım Dersleri/4-Redis/Redis_Cache_Example/Redis.Pub.Sub.Subscriber.Example/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+
+ISubscriber subscriber = connection.GetSubscriber();
+
+subscriber.Subscribe("myChannel", (channel, message) =>
+{
+    Console.WriteLine($"Gelen Mesaj: {message}");
+});
+Console.ReadLine();
+
+//kanal ismine myChannel.* yazarsam myChannel.1, myChannel.2 gibi kanalların hepsini dinler.
