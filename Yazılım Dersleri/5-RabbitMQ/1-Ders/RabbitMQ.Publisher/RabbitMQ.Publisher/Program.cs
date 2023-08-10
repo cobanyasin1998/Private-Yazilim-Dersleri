6b952ca6da40b5db71b0efd1151a42c9ccb34400
Yasin Çoban <cobanyasin1998@gmail.com>
2023-08-11 02:48:59 +0300

    Basic RabbitMQ

--- Yazılım Dersleri/5-RabbitMQ/1-Ders/RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
+++ Yazılım Dersleri/5-RabbitMQ/1-Ders/RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -1,14 +1,23 @@
 ﻿
 
 using RabbitMQ.Client;
+using System.Text;
 //Bağlantı Oluşturma
 ConnectionFactory factory = new();
-factory.Uri =new("amqps://gclmhric:ghEmFR2jAVVl7QB773LDPYRNgII5f3ad@toad.rmq.cloudamqp.com/gclmhric");
+factory.Uri = new("amqps://gclmhric:ghEmFR2jAVVl7QB773LDPYRNgII5f3ad@toad.rmq.cloudamqp.com/gclmhric");
 
 //Bağlantıyı Aktifleştirme ve Kanal Açma
 using IConnection connection = factory.CreateConnection();
-IModel channel= connection.CreateModel();
+IModel channel = connection.CreateModel();
 
 //Queue Oluşturma
-channel.QueueDeclare("example-queue");
+channel.QueueDeclare("example-queue", exclusive: false, autoDelete: false);
 
+//Queue'ya Mesaj Gönderme
+//RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir. Haliyle mesajları byte'a dönüştürmemiz gerekecektir.
+
+byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+
+channel.BasicPublish(exchange: "", "example-queue", body: message);
+
+Console.Read();
