7bb289010e80d760a01cf404e0478127ff395093
Yasin Çoban <cobanyasin1998@gmail.com>
2023-08-11 02:10:04 +0300

    rabbit mq lessons init

---
+++ Yazılım Dersleri/5-RabbitMQ/1-Ders/RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using RabbitMQ.Client;
+//Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri =new("amqps://gclmhric:ghEmFR2jAVVl7QB773LDPYRNgII5f3ad@toad.rmq.cloudamqp.com/gclmhric");
+
+//Bağlantıyı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+IModel channel= connection.CreateModel();
+
+//Queue Oluşturma
+channel.QueueDeclare("example-queue");
+
