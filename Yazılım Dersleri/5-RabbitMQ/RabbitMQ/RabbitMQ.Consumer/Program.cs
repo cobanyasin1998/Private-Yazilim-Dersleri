899992213051c27fc4fbbcf6901bd97411db8424
Yasin Çoban <cobanyasin1998@gmail.com>
2023-08-11 18:28:49 +0300

    rabbitmq

---
+++ Yazılım Dersleri/5-RabbitMQ/RabbitMQ/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Bağlantı Oluşturma
+ConnectionFactory factory = new ConnectionFactory();
+
+factory.Uri = new("amqps://gclmhric:ghEmFR2jAVVl7QB773LDPYRNgII5f3ad@toad.rmq.cloudamqp.com/gclmhric");
+//Bağlantı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue Oluşturma
+
+channel.QueueDeclare("example-queue", exclusive: false, autoDelete: false);
+
+
+//Queue'dan Mesaj Okuma
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", false, consumer);
+
+consumer.Received += (sender, e) =>
+{
+    //Kuyruğa gelen mesajin işlendiği yerdir
+    //e.Body : Kuyruktaki mesajın verisini bütünsel olarak getirir. ToArray yerisine Span da kullanılabilir.
+
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine(message);
+    channel.BasicAck(e.DeliveryTag, multiple: false);
+    //channel.BasicNack(e.DeliveryTag, multiple: false, requeue: true);
+
+};
+
+Console.Read();
---
+++ Yazılım Dersleri/5-RabbitMQ/RabbitMQ/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+//Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://gclmhric:ghEmFR2jAVVl7QB773LDPYRNgII5f3ad@toad.rmq.cloudamqp.com/gclmhric");
+
+//Bağlantıyı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+//Queue Oluşturma
+channel.QueueDeclare("example-queue", exclusive: false, autoDelete: false, durable: true);
+
+//Queue'ya Mesaj Gönderme
+//RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir. Haliyle mesajları byte'a dönüştürmemiz gerekecektir.
+
+byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(2000);
+    channel.BasicPublish(exchange: "", "example-queue", body: message, basicProperties: properties);
+}
+
+
+Console.Read();
