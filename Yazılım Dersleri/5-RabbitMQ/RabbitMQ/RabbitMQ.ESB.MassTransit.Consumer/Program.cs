18f5069dd51c86de8cd1af1b60f63cfa030106bc
Yasin Çoban <cobanyasin1998@gmail.com>
2023-08-13 02:40:59 +0300

    massTransit console application

---
+++ Yazılım Dersleri/5-RabbitMQ/RabbitMQ/RabbitMQ.ESB.MassTransit.Consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using MassTransit;
+using RabbitMQ.ESB.MassTransit.Consumer.Consumers;
+
+string rabbitMQUri = "amqps://gclmhric:ghEmFR2jAVVl7QB773LDPYRNgII5f3ad@toad.rmq.cloudamqp.com/gclmhric";
+
+string queueName = "example-queue";
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMQUri);
+    factory.ReceiveEndpoint(
+        queueName: queueName,
+        endpoint =>
+        {
+            endpoint.Consumer<ExampleMessageConsumer>();
+        });
+});
+
+await bus.StartAsync();
+
+Console.ReadLine();
---
+++ Yazılım Dersleri/5-RabbitMQ/RabbitMQ/RabbitMQ.ESB.MassTransit.Publisher/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+
+using MassTransit;
+using RabbitMQ.ESB.MassTransit.Shared.Messages;
+
+string rabbitMQUri = "amqps://gclmhric:ghEmFR2jAVVl7QB773LDPYRNgII5f3ad@toad.rmq.cloudamqp.com/gclmhric";
+
+string queueName = "example-queue-2";
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMQUri, conf =>
+    {
+        conf.Username("gclmhric");
+        conf.Password("ghEmFR2jAVVl7QB773LDPYRNgII5f3ad");
+    });
+});
+
+
+ISendEndpoint sendEndpoint = 
+    bus.GetSendEndpoint(new($"{rabbitMQUri}/{queueName}")).Result;
+
+
+ExampleMessage data = new ExampleMessage()
+{
+    Text = "Hello"
+};
+try
+{
+    await sendEndpoint.Send<IMessage>(data);
+
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+
+
+
+Console.ReadLine();
