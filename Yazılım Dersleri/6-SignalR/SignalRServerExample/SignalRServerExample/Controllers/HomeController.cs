fee3addbad63ff29738f8df38f796701eeccaf23
Yasin Çoban <cobanyasin1998@gmail.com>
2023-08-14 00:42:21 +0300

    rabbitmq finished, signalr started

---
+++ Yazılım Dersleri/6-SignalR/SignalRServerExample/SignalRServerExample/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRServerExample.Business;
+using SignalRServerExample.Hubs;
+using System.Threading.Tasks;
+
+namespace SignalRServerExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        readonly MyBusiness _myBusiness;
+        readonly IHubContext<MyHub> _hubContext;
+
+        public HomeController(MyBusiness myBusiness, IHubContext<MyHub> hubContext)
+        {
+            _myBusiness = myBusiness;
+            _hubContext = hubContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string message)
+        {
+            await _myBusiness.SendMessageAsync(message);
+            return Ok("İşlem Başarılı");
+        }
+    }
+}
---
+++ Yazılım Dersleri/6-SignalR/SignalRServerExample/SignalRServerExample/Startup.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using SignalRServerExample.Business;
+using SignalRServerExample.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRServerExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(opt => opt.AddDefaultPolicy(policy =>
+            {
+
+                policy.AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+                .SetIsOriginAllowed(origin => true);
+
+
+
+            }));
+            services.AddTransient<MyBusiness>(); 
+            services.AddSignalR();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SignalRServerExample", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SignalRServerExample v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseCors();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<MyHub>("/myHub");
+            });
+        }
+    }
+}
