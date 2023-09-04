0c2acd2736b3de142bda49c88df5655d03b3c289
Yasin Çoban <cobanyasin1998@gmail.com>
2023-09-04 23:55:32 +0300

    design_pattern_prototype

---
+++ Yazılım Dersleri/9-Design Patterns/Creational_Patterns/Prototype_Design_Pattern_Pratic/Program.cs
@@ -0,0 +1,43 @@
+﻿
+Person person1 = new Person("Yasin", "Çoban", Departmant.A, 100, 10);
+Person person2 = person1.Clone();
+person2.Name = "Meltem";
+person2.Salary = 500;
+//Person person2 = new Person("Meltem", "Çoban", Departmant.A, 500, 10);
+
+//Abstract Prototype
+interface IPersonClonable
+{
+    Person Clone();
+}
+
+//Concrete Prototype
+
+class Person : IPersonClonable
+{
+    public Person(string name, string surname, Departmant departmant, int salary, int premium)
+    {
+        Name = name;
+        Surname = surname;
+        Departmant = departmant;
+        Salary = salary;
+        Premium = premium;
+        Console.WriteLine("Person nesnesi üretildi");
+
+    }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public Departmant Departmant { get; set; }
+    public int Salary { get; set; }
+    public int Premium { get; set; }
+
+    public Person Clone()
+    {
+        return (Person)base.MemberwiseClone();
+    }
+}
+
+enum Departmant
+{
+    A, B, C
+}
---
+++ Yazılım Dersleri/9-Design Patterns/Creational_Patterns/Prototype_Design_Pattern_Pratic_2/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+
+Person person1 = new Person("Yasin", "Çoban", Departmant.A, 100, 10);
+Person person2 = (Person)person1.Clone();
+person2.Name = "Meltem";
+person2.Salary = 500;
+//Person person2 = new Person("Meltem", "Çoban", Departmant.A, 500, 10);
+
+
+
+
+class Person : ICloneable
+{
+    public Person(string name, string surname, Departmant departmant, int salary, int premium)
+    {
+        Name = name;
+        Surname = surname;
+        Departmant = departmant;
+        Salary = salary;
+        Premium = premium;
+        Console.WriteLine("Person nesnesi üretildi");
+
+    }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public Departmant Departmant { get; set; }
+    public int Salary { get; set; }
+    public int Premium { get; set; }
+
+
+   public object Clone()
+    {
+        return base.MemberwiseClone();
+    }
+}
+
+enum Departmant
+{
+    A, B, C
+}
