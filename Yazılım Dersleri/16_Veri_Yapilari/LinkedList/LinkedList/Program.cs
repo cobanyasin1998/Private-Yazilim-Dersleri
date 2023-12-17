d62a1d33cbdb9ccf9dd9a96f0afd74f3dafb7bda
Yasin Çoban <cobanyasin1998@gmail.com>
2023-12-18 00:55:28 +0300

    linkedlist data yapısı

--- Yazılım Dersleri/16_Veri_Yapilari/LinkedList/LinkedList/Program.cs
+++ Yazılım Dersleri/16_Veri_Yapilari/LinkedList/LinkedList/Program.cs
@@ -1,6 +1,26 @@
-﻿
+﻿//Data Structures => LinkedList
+using LinkedList;
+
+CustomLinkedList<int> list = new();
+
+list.AddFirst(1);
+list.AddLast(3);
+list.AddLast(new[] { 4, 5 });
+list.AddMiddle(5);
 
 
-//Data Structures => LinkedList
+
+Print();
+Console.ReadLine();
 
 
+
+
+void Print()
+{
+    foreach (var item in list)
+    {
+        Console.WriteLine(item);
+    }
+
+}
---
+++ Yazılım Dersleri/16_Veri_Yapilari/Queue_Stack/Queue_Stack/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//Data Structures => Queue & Stack
+
+
+/*
+ 
+Queue (Kuyruk) Örnekleri:
+--Fast Food Restoranı, Yazıcı Kuyruğu, Call Center
+
+Stack (Yığın) Örnekleri:
+--Web Tarayıcı Geçmişi, Program Çağrı Yığını Kitaplar (Fiziksel)
+ 
+ 
+ */
