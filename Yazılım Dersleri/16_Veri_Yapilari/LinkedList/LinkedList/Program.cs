87e373488e51fddac02e0b53457b86036027b1e6
Yasin Çoban <cobanyasin1998@gmail.com>
2023-12-17 18:46:09 +0300

    veri yapıları recursive-trie

---
+++ Yazılım Dersleri/16_Veri_Yapilari/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,6 @@
+﻿
+
+
+//Data Structures => LinkedList
+
+
---
+++ Yazılım Dersleri/16_Veri_Yapilari/Trie/Trie/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+//Data Structures => Trie
+
+
+
+Trie.Trie trie = new Trie.Trie();
+
+trie.Add("yasin");
+trie.Add("meltem");
+trie.Add("yasmin");
+trie.Print(trie);
