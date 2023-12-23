45c843101f66e120bdd8e7606a418092668c7fc8
Yasin Çoban <cobanyasin1998@gmail.com>
2023-12-24 01:56:12 +0300

    Sıralama Algoritmaları

---
+++ 17_SortingAlgorithms/InsertionSort/Program.cs
@@ -0,0 +1,28 @@
+﻿
+//Sıralama Algoritmaları - Insertion Sort (Ekleyerek Sıralama)
+
+int[] numbers = { 9, 4, 2, 7, 3, 1, 5, 8, 6 };
+string[] names = { "Ali", "Veli", "Ayşe", "Fatma", "Hayriye", "Hasan", "Hüseyin", "Hülya", "Hakkı", "Hikmet" };
+
+InsertionSort(numbers);
+InsertionSort(names);
+
+Console.WriteLine(string.Join(" ", numbers));
+Console.WriteLine(string.Join(" ", names));
+
+void InsertionSort<T>(T[] array) where T: IComparable
+{
+    for (int i = 1; i < array.Length; i++)
+    {
+        T temp = array[i];
+        int j = i - 1;
+
+        while (j >= 0 && array[j].CompareTo(temp) > 0)
+        {
+            array[j + 1] = array[j];
+            j--;
+        }
+
+        array[j + 1] = temp;
+    }
+}
---
+++ 17_SortingAlgorithms/SelectionSort/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// Sıralama Algoritmaları - Selection Sort (Seçerek Sıralama)
+
+
+int[] numbers = { 9, 4, 2, 7, 3, 1, 5, 8, 6 };
+string[] names = { "Ali", "Veli", "Ayşe", "Fatma", "Hayriye", "Hasan", "Hüseyin", "Hülya", "Hakkı", "Hikmet" };
+SelectionSort(numbers);
+SelectionSort(names);
+Console.WriteLine(string.Join(" ", numbers));
+Console.WriteLine(string.Join(" ", names));
+void SelectionSort<T>(T[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minIndex = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (Comparer<T>.Default.Compare(array[j], array[minIndex]) < 0)
+            {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i)
+        {
+            T temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+    }
+}
