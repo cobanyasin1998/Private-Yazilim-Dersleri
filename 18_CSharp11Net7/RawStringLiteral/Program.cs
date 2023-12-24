4e6a5d9488496365b3e91d02ff4ce0e8517ece3a
Yasin Çoban <cobanyasin1998@gmail.com>
2023-12-24 14:10:11 +0300

    raw string literal

--- 18_CSharp11Net7/RawStringLiteral/Program.cs
+++ 18_CSharp11Net7/RawStringLiteral/Program.cs
@@ -1,2 +1,42 @@
 ﻿
 
+//String
+string firstName = "Yasin";
+string lastName = "ÇOBAN";
+
+string fullName = firstName + " " + lastName;
+Console.WriteLine(fullName);
+
+Console.WriteLine(string.Concat(firstName, ' ', lastName));
+
+//string.Format()
+
+string fullName2 = string.Format("{0} {1}", firstName, lastName);
+
+Console.WriteLine(fullName2);
+
+//String Interpolation
+
+string fullName3 = $"{firstName} {lastName}";
+Console.WriteLine(fullName3);
+string name = @" ""Yasin"" ";
+//String Escape Characters
+
+string name2 = " \"Yasin\" ";
+
+//RawStringLiteral
+string json = string.Empty;
+json = $$"""
+{
+    "firstName": {{firstName}},
+    "lastName": {{lastName}},
+    "age": 25,
+    "isMarried": true,
+    "address": {
+        "street": "123 Main St",
+        "city": "Ankara",
+        "state"": "TR"
+    }
+}
+""";
+Console.WriteLine(json);
