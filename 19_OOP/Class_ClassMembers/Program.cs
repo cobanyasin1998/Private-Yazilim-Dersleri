4f660a06d137ebed915cd8fbdc7a188ee951e839
Yasin Çoban <cobanyasin1998@gmail.com>
2023-12-25 20:42:58 +0300

    elasticsearch e giriş

---
+++ 19_OOP/Class_ClassMembers/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+#region Class Members
+
+#region Field
+//Nesne içerisinde değer tutmamızı sağlayan alanlardır.
+MyClass m1 = new MyClass();
+MyClass m2 = new MyClass();
+m1.a = 10;
+m2.a = 20;
+
+
+#endregion
+
+
+#endregion
+
+
+public class MyClass
+{
+    public int a; //Field
+    public string b; //Field
+}
