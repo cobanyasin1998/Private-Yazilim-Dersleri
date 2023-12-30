19c97911f0f9767199d50986ff474c2b230d07f7
Yasin Çoban <cobanyasin1998@gmail.com>
2023-12-30 23:33:05 +0300

    oop continue

---
+++ 19_OOP/Derinlemesine_Encapsulation/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+MyClass myClass = new MyClass();
+myClass.ASet(5);
+Console.WriteLine(myClass.AGet());//Eski Yöntem
+
+
+
+
+class MyClass()
+{
+    int a;
+
+    public int AGet()
+    {
+        return this.a;
+    }
+    public void ASet(int val)
+    {
+        this.a = val;
+    }
+}
+class MyClass2()
+{
+    //propfull
+    private int yas;
+
+    public int Yas
+    {
+        get { return yas; }
+        set { yas = value; }
+    }
+
+}
---
+++ 19_OOP/HeapStack_Kritik/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+MyClass myClass = null;
+myClass.MyProperty = 1;//NullReferenceException
+
+new MyClass().Topla(1, 2);//Garbage Collector
+
+new MyClass()
+{
+    MyProperty = 1,
+    MyProperty1 = 2,
+    MyProperty2 = 3,
+    MyProperty3 = 4
+
+};
+
+class MyClass
+{
+    public int MyProperty { get; set; }
+    public int MyProperty1 { get; set; }
+    public int MyProperty2 { get; set; }
+    public int MyProperty3 { get; set; }
+
+    public int Topla(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+
+
+
