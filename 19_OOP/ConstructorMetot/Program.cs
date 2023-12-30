9701d94f777273b992241fab6e3a8a11d16c7012
Yasin Çoban <cobanyasin1998@gmail.com>
2023-12-31 02:30:33 +0300

    oop continue

---
+++ 19_OOP/ConstructorMetot/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+
+//Constructor (Yapıcı Metotlar)
+
+MyClass myClass = new MyClass(); //Default Constructor metot çalışır
+MyClass myClass2 = new MyClass(5); //Parametreli Constructor metot çalışır
+
+//MyClass2 myClass3 = new MyClass2(); //Hata verir. Çünkü MyClass2 sınıfının Constructor metodu private olarak tanımlanmıştır.
+
+
+new thisKeywordIleConstructorGecisler();
+new thisKeywordIleConstructorGecisler(2);
+
+class MyClass
+{
+    public MyClass()
+    {
+        //Default Constructor metot
+    }
+    public MyClass(int a)
+    {
+        //Parametreli Constructor metot
+    }
+
+}
+
+public class MyClass2
+{
+    private MyClass2()
+    {
+        //Private Constructor metot
+    }
+    public void X()
+    {
+        new MyClass2(); //Bu şekilde çağırılabilir.
+    }
+}
+
+
+class thisKeywordIleConstructorGecisler
+{
+    public thisKeywordIleConstructorGecisler()
+    {
+        Console.WriteLine("1. Constructor");
+
+
+    }
+    public thisKeywordIleConstructorGecisler(int a) : this()
+    {
+        Console.WriteLine("2. Constructor");
+
+    }
+    public thisKeywordIleConstructorGecisler(int a,int b) : this(a)
+    {
+        Console.WriteLine("3. Constructor");
+
+    }
+}
--- 19_OOP/Records/Program.cs
+++ 19_OOP/Records/Program.cs
@@ -1,4 +1,67 @@
 ﻿
+// Init only properties
 
+MyClass myClass = new MyClass()
+{
+    //MyProperty = 3 // Error
+    MyProperty1 = 3 // OK //Init only property
+};
+MyClass myClass2 = new MyClass()
+{
+    //MyProperty = 3 // Error
+    MyProperty1 = 3 // OK //Init only property
+};
+//myClass.MyProperty1 = 4; // Error
+
+Console.WriteLine(myClass.Equals(myClass2));// False
+
+//Records
+
+Person person1 = new() { FirstName = "John", LastName = "Doe" };
+Person person2 = new() { FirstName = "John", LastName = "Doe" };
+
+
+Console.WriteLine(person1.Equals(person2));  // True
+
+
+
+
+//With Expression
+
+Person person3 = person1 with { FirstName = "Jane" };
+
+
+
+
+
+class MyClass
+{
+    public int MyProperty { get; } = 3;
+    public int MyProperty1 { get; init; }
+}
+
+
+
+
+public record Person
+{
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+}
+
+
+
+/*
+ Önemli Not
+
+
++Class'lar da verisel olarak nesne ön plandadır ve bir farklı referansa sahip olan nesne farklı değer olarak algılanmaktadır.
++Dolayısıyla Equals metodu ile karşılaştırıldığında false dönmektedir.
+ 
+ 
+
++Record'lar da ise veri ön plandadır ve bir farklı referansa sahip olan nesne aynı değer olarak algılanmaktadır.
++Dolayısıyla Equals metodu ile karşılaştırıldığında true dönmektedir.
+ */
 
 
