a7fe17d936d7c00918525a2d782f46b7aa0506f0
Yasin Çoban <cobanyasin1998@gmail.com>
2023-12-31 16:37:32 +0300

    oop continue

---
+++ 19_OOP/Deconstruct_Metot/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+Person person = new Person { FirstName = "Ali", Age = 25 };
+var (name, age) = person;
+
+
+
+class Person
+{
+    public required string FirstName { get; set; }
+    public int Age { get; set; }
+
+    public void Deconstruct(out string name, out int age)
+    {
+        name = FirstName;
+        age = Age;
+    }
+}
--- 19_OOP/Destructor_Metot/Program.cs
+++ 19_OOP/Destructor_Metot/Program.cs
@@ -2,3 +2,28 @@
 
 // Destructor Metot
 
+X();
+GC.Collect();
+GC.WaitForPendingFinalizers();
+Console.ReadLine(); 
+
+static void X()
+{
+    MyClass myClass = new MyClass();
+}
+
+class MyClass
+{
+
+    public MyClass()
+    {
+        // Constructor Metot
+        Console.WriteLine("Constructor Metot");
+    }
+
+    ~MyClass()
+    {
+        // Destructor Metot
+        Console.WriteLine("Destructor Metot");
+    }
+}
