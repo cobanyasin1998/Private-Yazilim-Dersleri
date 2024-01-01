d60e8b1cb094df245f6049a6d629212b70f93ccf
Yasin Çoban <cobanyasin1998@gmail.com>
2024-01-01 15:44:19 +0300

    oop continue

--- 19_OOP/Inheritance/Program.cs
+++ 19_OOP/Inheritance/Program.cs
@@ -1,3 +1,33 @@
 ﻿
 
+Muhasebeci muhasebeci = new Muhasebeci();
+muhasebeci.Adi = "Mehmet";
+muhasebeci.Soyadi = "Kaya";
+muhasebeci.MedeniHali = MedeniDurum.Bekar;
+muhasebeci.Musavir = true;
 
+class Personel
+{
+    public string Adi { get; set; }
+    public string Soyadi { get; set; }
+    public MedeniDurum MedeniHali { get; set; }
+}
+
+class Muhasebeci : Personel
+{
+    public bool Musavir { get; set; }
+}
+
+class Yazilimci : Personel
+{
+    public string[] KullandigiDiller { get; set; }
+}
+class Mudur : Personel
+{
+  
+}
+enum MedeniDurum
+{
+    Bekar = 1,
+    Evli = 2
+}
---
+++ 19_OOP/Inheritance_2/Program.cs
@@ -0,0 +1,97 @@
+﻿
+
+
+D d = new D();
+
+MyClass2 myClass = new MyClass2();
+
+
+class BuyukBaba
+{
+
+}
+class Baba : BuyukBaba
+{
+
+}
+class Anne : Baba
+{
+
+}
+class Cocuk :  Anne
+{
+
+}
+
+
+
+
+
+
+
+
+class A
+{
+    public A()
+    {
+        Console.WriteLine("A");
+    }
+   
+}
+class B : A
+{
+    public B()
+    {
+        Console.WriteLine("B");
+    }
+
+}
+class C : B
+{
+    public C()
+    {
+        Console.WriteLine("C");
+    }
+}
+class D : C
+{
+    public D()
+    {
+        Console.WriteLine("D");
+    }
+}
+
+
+
+
+class MyClass
+{
+    public MyClass(int a)
+    {
+        Console.WriteLine("MyClass");
+    }
+}
+class MyClass2 : MyClass
+{
+    public MyClass2():base(5)
+    {
+        Console.WriteLine("MyClass2");
+    }
+}
+
+class AClass
+{
+    int a;
+    public int b;
+    public int MyProperty { get; set; }
+}
+class BClass : AClass
+{
+
+    int c;
+    public void X()
+    {
+        MyProperty = 123;
+        this.b = 123;
+    }
+}
