f6ed6b316270662726077cc423f9f12db965f100
Yasin Çoban <cobanyasin1998@gmail.com>
2024-01-05 17:17:20 +0300

    oop continue

--- 19_OOP/Interface/Program.cs
+++ 19_OOP/Interface/Program.cs
@@ -1,2 +1,65 @@
 ﻿
 
+using System.Linq.Expressions;
+
+interface IReadRepository<T>
+{
+    T GetById(int id);
+    IEnumerable<T> List();
+    IEnumerable<T> List(Expression<Func<T, bool>> predicate);
+}
+interface IWriteRepository<T>
+{
+    void Add(T entity);
+    void Delete(T entity);
+    void Update(T entity);
+}
+interface IRepository<T> : IReadRepository<T>, IWriteRepository<T>
+{
+
+}
+ interface IProductRepository : IRepository<Product>
+{
+
+}
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+public abstract class BaseRepository<T> : IRepository<T>
+{
+    public virtual void Add(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual void Delete(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual T GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual IEnumerable<T> List()
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual IEnumerable<T> List(Expression<Func<T, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual void Update(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
+public class ProductRepository : BaseRepository<Product>, IProductRepository
+{
+   
+}
