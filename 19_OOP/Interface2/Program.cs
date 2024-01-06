d51d78e94192ad40bab886596527d22c935e6bce
Yasin Çoban <cobanyasin1998@gmail.com>
2024-01-06 23:38:57 +0300

    oop continue

--- 19_OOP/Interface2/Program.cs
+++ 19_OOP/Interface2/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿interface IArea
+{
+    double Calculate();
+}
+interface IPerimeter
+{
+    double Calculate();
+}
+class Calculator : IArea, IPerimeter
+{
+    //Explicit interface member implementation:
+    double IPerimeter.Calculate()
+    {
+        return 0;
+    }
+
+    double IArea.Calculate()
+    {
+        return 0;
+    }
+}
+
+//Default interface implementation:C# 8.0
+interface IAreaa
+{
+    double Calculate()
+    {
+        return 0;
+    }
+}
+class Calculatorr : IAreaa
+{
+    //No need to implement Calculate() method
+}
